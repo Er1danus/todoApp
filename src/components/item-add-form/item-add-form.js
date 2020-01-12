0470b62965edbda22d4f27d3a5fdbbe8cf7bf365
Talgat <duzbatyrovtalgat96@gmail.com>
2020-01-12 23:58:05 +0400

    Форма для добавления дел

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,12 +4,36 @@ import './item-add-form.css'
 
 export default class ItemAddForm extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      label: ''
+    }
+
+    this.onLabelChange = (e) => {
+      this.setState({
+        label: e.target.value
+      })
+    }
+
+    this.onSubmit = (e) => {
+      e.preventDefault();
+      this.props.onItemAdded(this.state.label)
+    }
+  }
+
   render() {
     return (
-      <div className='item-add-form'>
-        <button className='btn btn-outline-secondary'
-        onClick={() => this.props.onItemAdded('hey')}>Add</button>
-      </div>
+      <form className='item-add-form d-flex'
+        onSubmit={this.onSubmit}>
+        <input type='text' className='form-control'
+          onChange={this.onLabelChange}
+          placeholder='what needs to be done'/>
+        <button className='btn btn-outline-secondary'>
+          Add
+        </button>
+    </form>
     )
   }
 }
